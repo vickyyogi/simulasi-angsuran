11828b5c4632803a528a8936e896ae69ac4a15ca
vickyyogi <pratamakyogi@gmail.com>
2026-07-25 17:50:26 +0700

    fixing error

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
-import './index.css'
+import React from 'react'
 import Header from './components/Header'
-import Deposito from './components/Deposito';
-import { Routes , Route } from 'react-router-dom';
-import Kredit from './components/Kredit';
+import Deposito from './components/Deposito'
+import { Routes , Route } from 'react-router-dom'
+import Kredit from './components/Kredit'
+import './index.css'
+
+
 
 function App() {
   return (
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,6 @@
 import './style.comp.css'
 import LogoArc from '../assets/logo-arc.png'
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter, Router, Routes, Route, Link } from 'react-router-dom';
 
 function Header() {
   return (
@@ -21,13 +21,13 @@ function Header() {
         {/* Social Media Links */}
         <div className="flex items-center space-x-4 text-gray-500">
             <a href="#" className="hover:text-blue-600 transition-colors">
-              <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 24 24"><path d="M22 12c0-5.523-4.477-10-10-10S2 6.477 2 12c0 4.991 3.657 9.128 8.438 9.878v-6.987h-2.54V12h2.54V9.797c0-2.506 1.492-3.89 3.777-3.89 1.094 0 2.238.195 2.238.195v2.46h-1.26c-1.243 0-1.63.771-1.63 1.562V12h2.773l-.443 2.89h-2.33v6.988C18.343 21.128 22 16.991 22 12z"/></svg>
+              Facebook
             </a>
             <a href="#" className="hover:text-pink-600 transition-colors">
-              <svg className="w-5 h-5" fill="none" stroke="currentColor" stroke-width="2" viewBox="0 0 24 24"><rect width="20" height="20" x="2" y="2" rx="5" ry="5"></rect><path d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zM17.5 6.5h.01"></path></svg>
+              Instagram
             </a>
             <a href="#" className="hover:text-black transition-colors">
-              <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 24 24"><path d="M18.244 2.25h3.308l-7.227 8.26 8.502 11.24H16.17l-5.214-6.817L4.99 21.75H1.68l7.73-8.835L1.254 2.25H8.08l4.713 6.231zm-1.161 17.52h1.833L7.084 4.126H5.117z"/></svg>
+              Twitter
             </a>
         </div>
       </div>

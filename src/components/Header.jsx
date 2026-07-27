fafaf18f8d327be75728b464f2558a76866ed8bf
vickyyogi <pratamakyogi@gmail.com>
2026-07-27 13:11:30 +0700

    cek

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,36 +1,62 @@
 import './style.comp.css'
 import LogoArc from '../assets/logo-arc.png'
 import { BrowserRouter, Router, Routes, Route, Link } from 'react-router-dom';
+import {useState} from 'react'
 
 function Header() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
   return (
-    <header className='bg-white border-b border-gray-200'>
+    <header className='bg-white shadow-md fixed w-full top-0 left-0 z-50'>
       <div className='flex items-center justify-between max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 h-16'>
-        {/* Logo */}
+        {/* Logo Desktop*/}
         <div className='p-4 shrink-0 flex items-center gap-3'>
             <img src={LogoArc} className='h-8 w-auto'/>
             <h2 className='text-blue-950 font-bold tracking-tight'>BPR Adiartha Reksacitra Grup</h2>
         </div>
 
-        {/* Navigation Links */}
-        <nav className='flex items-center space-x-4'>
+        {/* Navigation Links Desktop */}
+        <nav className='hidden md:flex items-center space-x-4'>
           <Link to="/" className='text-gray-700 hover:text-blue-600 transition-colors'>Simulasi Kredit</Link>
           <Link to="/deposito" className='text-gray-700 hover:text-blue-600 transition-colors'>Simulasi Deposito</Link>
         </nav>
         
-        {/* Social Media Links */}
+        {/* Social Media Links Desktop */}
         <div className="flex items-center space-x-4 text-gray-500">
             <a href="#" className="hover:text-blue-600 transition-colors">
-              Facebook
+              
             </a>
             <a href="#" className="hover:text-pink-600 transition-colors">
-              Instagram
+              
             </a>
             <a href="#" className="hover:text-black transition-colors">
-              Twitter
+              
             </a>
         </div>
+
+        {/* Tombol Hamburger */}
+        <div className='flex md:hidden items-center'>
+            <button onClick={() => setIsMenuOpen(!isMenuOpen)} type='button' className='text-gray-700 hover:text-indigo-600 focus:outline-none' aria-label='Toggle-menu'>
+              {isMenuOpen ? (
+                <svg className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 18L18 6M6 6l12 12" />
+                </svg>
+              ) : ( <svg className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16M4 18h16" />
+                </svg> )}
+            </button>
+        </div>
       </div>
+      {/* menu mobile dropdown */}
+            <div className={`${isMenuOpen ? 'block' : 'hidden'} md:hidden bg-gray-50 border-t border-gray-200 shadow-inner`}>
+                <div className='px-2 pt-2 pb-3 space-y-1 sm:px-2'>
+                  <Link to="/" className='block px-3 y-2 text-gray-700 hover:text-blue-600 transition-colors'>Simulasi Kredit</Link>
+                  <Link to="/deposito" className='block px-3 y-2 text-gray-700 hover:text-blue-600 transition-colors'>Simulasi Deposito</Link>
+                  <a href="#" className="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:bg-indigo-50 hover:text-indigo-600">Facebook</a>
+                  <a href="#" className="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:bg-indigo-50 hover:text-indigo-600">Instagram</a>
+                  <a href="#" className="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:bg-indigo-50 hover:text-indigo-600">Twitter</a>
+                </div>
+            </div>
     </header>
   )
 }

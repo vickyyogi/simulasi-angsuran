9fff406882242a4c6bedc76a604616d32082d3e2
vickyyogi <pratamakyogi@gmail.com>
2026-07-27 09:09:38 +0700

    2

--- src/components/Kredit.jsx
+++ src/components/Kredit.jsx
@@ -8,13 +8,15 @@ function Kredit(){
         interestRate:'1.5',
         admFee: '2',
         notarisFee:'0',
-        materaiFee:'0'
+        materaiFee:'0',
+        lainnyaFee:'0',
+        pajakStnkFee:'0'
     });
     return (
-        <div className="grid md:grid-cols-3 gap-4 p-4">
+        <div className="grid md:grid-cols-4 gap-4 p-4">
             <div className=" bg-gradient-to-br bg-indigo-900/90 to-slate-900/50 border border-slate-700/50 p-4 rounded-lg shadow-md space-y-6">
                 <h2 className="text-2xl font-semibold mb-2 text-blue-50">Simulasi Kredit</h2>
-                <form className="space-y-4 text-white">
+                <form className="space-y-4 text-white text-sm">
                     
                         <label htmlFor="loanAmount" className="block text-sm font-medium ">Jumlah Kredit (Rp)</label>
                         <input type="number" name="loanAmount" value={formData.loanAmount} onChange={(e) => setFormData({...formData, loanAmount: e.target.value})} className="mt-1 block w-full border border-gray-300 rounded-md shadow-sm focus:ring-blue-500 focus:border-blue-500 sm:text-sm py-2 pl-2" />
@@ -26,12 +28,16 @@ function Kredit(){
                         <input type="number" name="notarisFee" value={formData.notarisFee} onChange={(e) => setFormData({...formData, notarisFee: e.target.value})} className="mt-1 block w-full border border-gray-300 rounded-md shadow-sm focus:ring-blue-500 focus:border-blue-500 sm:text-sm py-2 pl-2" />
                         <label htmlFor="loanAmount" className="block text-sm font-medium ">Biaya Materai (Rp)</label>
                         <input type="number" name="materaiFee" value={formData.materaiFee} onChange={(e) => setFormData({...formData, materaiFee: e.target.value})} className="mt-1 block w-full border border-gray-300 rounded-md shadow-sm focus:ring-blue-500 focus:border-blue-500 sm:text-sm py-2 pl-2" />
+                        <label htmlFor="loanAmount" className="block text-sm font-medium ">Biaya Lainnya (Rp)</label>
+                        <input type="number" name="lainnyaFee" value={formData.lainnyaFee} onChange={(e) => setFormData({...formData, lainnyaFee: e.target.value})} className="mt-1 block w-full border border-gray-300 rounded-md shadow-sm focus:ring-blue-500 focus:border-blue-500 sm:text-sm py-2 pl-2" />
+                        <label htmlFor="loanAmount" className="block text-sm font-medium ">Biaya Pajak/STNK (Rp)</label>
+                        <input type="number" name="pajakStnkFee" value={formData.pajakStnkFee} onChange={(e) => setFormData({...formData, pajakStnkFee: e.target.value})} className="mt-1 block w-full border border-gray-300 rounded-md shadow-sm focus:ring-blue-500 focus:border-blue-500 sm:text-sm py-2 pl-2" />
                     
                     <button type="submit" className="w-full bg-white text-indigo-950 py-2 px-4 rounded-md hover:bg-indigo-600 hover:text-white transition-colors cursor-pointer" >Hitung</button>
                 </form>
             </div>
-            <div className="">
-                <h2>Hasil</h2>
+            <div className="bg-gradient-to-br bg-indigo-900/90 to-slate-900/50 border border-slate-700/50 p-4 rounded-lg shadow-md space-y-6 text-white">
+                <h2 className="text-2xl font-semibold mb-2">Hasil PA</h2>
             </div>
         </div>
     );
